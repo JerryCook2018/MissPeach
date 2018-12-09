8fc4e93c01247a4e527b75093e15b84335e71bef
JerryCook2018 <jcook48@baker.edu>
2018-12-09 16:19:50 -0500

    fixed decimal places in report.

--- MissPeach/WeeklyReport.cs
+++ MissPeach/WeeklyReport.cs
@@ -32,18 +32,18 @@ namespace MissPeach
             Console.Out.WriteLine();
             Console.Out.WriteLine("   Earnings            rate              hours                this period         year to date");
             Console.Out.WriteLine("   ----------------------------------------------------------------------         ------------");
-            Console.Out.WriteLine("   Regular             {0}                                    {1}                    {3}", payCheck.GetGrossPay(), payCheck.GetGrossPay(), payCheck.GetGrossPay());
-            Console.Out.WriteLine("               Gross Pay                                      {0}                    {2}", payCheck.GetGrossPay(), payCheck.GetGrossPay());
+            Console.Out.WriteLine("   Regular             {0}                                    {1}                    {3}", payCheck.GetGrossPay().ToString("C2"), payCheck.GetGrossPay().ToString("C2"), payCheck.GetGrossPay().ToString("C2");
+            Console.Out.WriteLine("               Gross Pay                                      {0}                    {2}", payCheck.GetGrossPay().ToString("C2"), payCheck.GetGrossPay().ToString("C2");
             Console.Out.WriteLine();
             Console.Out.WriteLine("   Deductions  Statutory");
-            Console.Out.WriteLine("               Federal Income Tax                            -{0}                    {1}", payCheck.GetFederalTax(), payCheck.GetFederalTax());
-            Console.Out.WriteLine("               Social Security Tax                           -{0}                    {1}", payCheck.GetSocialTax(), payCheck.GetSocialTax());
-            Console.Out.WriteLine("               Medicare Tax                                  -{0}                    {1}", payCheck.GetMedicareTax(), payCheck.GetMedicareTax());
-            Console.Out.WriteLine("               MI State Income Tax                           -{0}                    {1}", payCheck.GetStateTax(), payCheck.GetStateTax());
+            Console.Out.WriteLine("               Federal Income Tax                            -{0}                    {1}", payCheck.GetFederalTax().ToString("F"), payCheck.GetFederalTax().ToString("F"));
+            Console.Out.WriteLine("               Social Security Tax                           -{0}                    {1}", payCheck.GetSocialTax().ToString("F"), payCheck.GetSocialTax().ToString("F"));
+            Console.Out.WriteLine("               Medicare Tax                                  -{0}                    {1}", payCheck.GetMedicareTax().ToString("F"), payCheck.GetMedicareTax().ToString("F"));
+            Console.Out.WriteLine("               MI State Income Tax                           -{0}                    {1}", payCheck.GetStateTax().ToString("F"), payCheck.GetStateTax().ToString("F"));
             Console.Out.WriteLine();
             Console.Out.WriteLine("               Other");
             Console.Out.WriteLine("               ----------------------------------------------------------");
-            Console.Out.WriteLine("               Direct Deposit                                -{0}                    {1}", payCheck.GetDirectDeposit(), payCheck.GetDirectDeposit());
+            Console.Out.WriteLine("               Direct Deposit                                -{0}                    {1}", payCheck.GetDirectDeposit().ToString("C2"), payCheck.GetDirectDeposit().ToString("C2"));
             Console.Out.WriteLine();
             Console.Out.WriteLine("               Net Pay                                        $0.00");
         
